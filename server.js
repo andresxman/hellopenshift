f5964dff3393f4482367377d5368de388db5d149
andresxman <41528431+andresxman@users.noreply.github.com>
2018-07-30 01:17:34 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-//var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
