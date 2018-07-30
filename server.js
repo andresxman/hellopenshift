b9defdfefb2361b1021d4f30442a1e3c06011b82
andresxman <41528431+andresxman@users.noreply.github.com>
2018-07-30 01:04:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-var port = process.env.PORT || 8080;
+//var port = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Fofim\n");
 }).listen(port);
 
-console.log("Server running on port " + port);
+//console.log("Server running on port " + port);
